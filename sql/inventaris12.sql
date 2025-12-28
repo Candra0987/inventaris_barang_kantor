dc2672ab92b82f39b066a53109b35fe6f72adb49
Candra0987 <As944859@gmail.com>
2025-12-28 21:37:30 +0700

    update

--- sql/inventaris12.sql
+++ sql/inventaris12.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 24, 2025 at 03:05 PM
+-- Generation Time: Dec 28, 2025 at 03:35 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -64,7 +64,8 @@ INSERT INTO `employees` (`id`, `name`, `email`, `password`, `role`, `created_at`
 (1, 'admin', 'admin@gmail.com', '$2y$10$VI0kQym09ew7kgHjmJ7dJ.t94ZdEmb5zLaDexOyus7Hj6/2JOHBxu', 'admin', '2025-11-29 12:23:49'),
 (2, 'alex', 'alex@gmail.com', '$2y$10$JwsQyrNErDtBaljdOrxXxeS9j0LuqRVBrmY.yHiky9alpNkPGq51K', 'karyawan', '2025-12-01 02:23:18'),
 (3, 'uhgyfy', 'ggyg@gmail.com', '$2y$10$jcTzI/GzygcDPHWP/M64N.rNtD7xcQAoxnRBfclfaGFu0po.o6yqK', 'karyawan', '2025-12-07 15:13:32'),
-(4, 'Adipati Candra', 'adipati@gmail', '$2y$10$j/Q97my3nhkyruKZhhfB1e34YloXJ4.5WP7JXmx63FxjZ95/JDXZG', 'karyawan', '2025-12-08 02:44:39');
+(4, 'Adipati Candra', 'adipati@gmail', '$2y$10$j/Q97my3nhkyruKZhhfB1e34YloXJ4.5WP7JXmx63FxjZ95/JDXZG', 'karyawan', '2025-12-08 02:44:39'),
+(5, 'asa', 'asa@gmail', '$2y$10$aIF1tKyGYi7LUNRZpTTsHOqWoXy4DAVAzZwMfSvLTKfJk/myNVpQq', 'karyawan', '2025-12-27 14:38:50');
 
 -- --------------------------------------------------------
 
@@ -86,17 +87,14 @@ CREATE TABLE `items` (
 --
 
 INSERT INTO `items` (`id`, `category_id`, `name`, `quantity`, `description`, `condition`) VALUES
-(1, 1, 'Pulpen', 59, 'ok', 'bagus'),
-(2, 1, 'Kertas', 758, 'ok', 'bagus'),
-(4, 1, 'aaaa', 1, 'sdf', 'bagus'),
-(6, 1, 'Cortape', 12, 'sdf', 'rusak'),
+(1, 1, 'Pulpen', 58, 'ok', 'bagus'),
+(2, 1, 'Kertas', 733, 'ok', 'bagus'),
+(6, 1, 'Cortape', 6, 'sdf', 'bagus'),
 (7, 1, 'Tinta', 9, 'ok', 'bagus'),
-(8, 1, 'rrrrrrrrrrr', 122, 'asdfdsf', 'rusak'),
-(10, 1, 'weqwfwe', 122, 'dewwe', 'bagus'),
-(11, 1, 'i7y', 1, 'jiuj', 'rusak'),
+(8, 2, 'keyboard', 122, 'rusak', 'rusak'),
+(10, 2, 'mouse', 119, 'ok', 'bagus'),
 (12, 2, 'Laptop', 5, 'ok', 'bagus'),
-(13, 3, 'Mobil', 5, 'ok', 'rusak'),
-(14, 1, 'abc', 1, 'abc', 'bagus');
+(13, 3, 'Mobil', 5, 'ok', 'rusak');
 
 -- --------------------------------------------------------
 
@@ -119,7 +117,6 @@ CREATE TABLE `loans` (
 --
 
 INSERT INTO `loans` (`id`, `employee_id`, `item_id`, `quantity`, `status`, `requested_at`, `processed_at`) VALUES
-(1, 2, 7, 1, 'returned', '2025-12-01 02:34:34', '2025-12-01 18:41:21'),
 (2, 2, 12, 2, 'returned', '2025-12-07 16:44:44', '2025-12-07 23:52:03'),
 (3, 2, 13, 2, 'returned', '2025-12-08 02:47:09', '2025-12-08 09:48:07'),
 (4, 2, 1, 6, 'approved', '2025-12-24 11:52:57', '2025-12-24 18:54:35'),
@@ -129,7 +126,22 @@ INSERT INTO `loans` (`id`, `employee_id`, `item_id`, `quantity`, `status`, `requ
 (8, 2, 7, 3, 'approved', '2025-12-24 11:53:48', '2025-12-24 18:54:32'),
 (9, 2, 2, 242, 'approved', '2025-12-24 11:55:04', '2025-12-24 18:56:53'),
 (10, 2, 1, 23, 'approved', '2025-12-24 11:56:02', '2025-12-24 18:56:52'),
-(11, 2, 12, 3, 'approved', '2025-12-24 11:56:14', '2025-12-24 18:56:51');
+(11, 2, 12, 3, 'approved', '2025-12-24 11:56:14', '2025-12-24 18:56:51'),
+(12, 2, 12, 2, 'pending', '2025-12-25 11:39:51', NULL),
+(13, 2, 2, 222, 'pending', '2025-12-25 11:41:21', NULL),
+(14, 2, 1, 22, 'pending', '2025-12-25 11:41:35', NULL),
+(15, 2, 2, 23, 'pending', '2025-12-25 11:45:55', NULL),
+(16, 2, 2, 23, 'pending', '2025-12-25 11:46:11', NULL),
+(17, 2, 2, 232, 'pending', '2025-12-25 11:47:18', NULL),
+(18, 2, 2, 22, 'pending', '2025-12-25 11:51:07', NULL),
+(19, 2, 2, 25, 'approved', '2025-12-25 11:53:46', '2025-12-25 19:31:23'),
+(20, 2, 2, 24, 'pending', '2025-12-25 11:54:06', NULL),
+(21, 2, 2, 23, 'pending', '2025-12-25 12:00:30', NULL),
+(22, 2, 1, 1, 'approved', '2025-12-26 14:22:32', '2025-12-26 21:31:10'),
+(23, 2, 6, 2, 'pending', '2025-12-27 09:01:50', NULL),
+(24, 2, 6, 2, 'rejected', '2025-12-27 09:27:03', '2025-12-27 16:29:07'),
+(25, 2, 6, 4, 'returned', '2025-12-27 09:28:00', '2025-12-27 16:29:58'),
+(26, 2, 10, 3, 'pending', '2025-12-27 13:32:23', NULL);
 
 --
 -- Indexes for dumped tables
@@ -171,25 +183,25 @@ ALTER TABLE `loans`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `loans`
 --
 ALTER TABLE `loans`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- Constraints for dumped tables
